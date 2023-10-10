560ef1e304a100182d1354c9017e24db9d3218a9
mkluszczynski <mkluszczynski07@gmail.com>
2023-10-10 12:04:34 +0200

    main update

--- __main__.py
+++ __main__.py
@@ -1,15 +1,22 @@
 from cafe.CafeRepository import CafeRepository
+from cafe.CafeService import CafeService
 
 file = open("data/MRN_OSM_amenity_cafe_4326_Point.csv")
 
 cafeRepo = CafeRepository(file)
+cafeService = CafeService(cafeRepo)
 
-for cafe in cafeRepo.find({"select": ["name", "smoking"]}):
-    if cafe["smoking"] != '':
-        print(cafe)
+for cafe in cafeService.getSmokeCount():
+    print(cafe)
 
+for payment in cafeService.getPaymentMethods():
+    print(payment)
 
 #ilość danych kawiarni
 #ilość dot. możliwości palenia
-#
+#ilość akceptowanych metod płatności
+#godziny otwarcia
+#ilość kawiarni w danym mieście
+
+
 
